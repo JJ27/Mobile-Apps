153d6957d08374fd567772c87349945f456149a7
JJ27 <66833672+JJ27@users.noreply.github.com>
2022-01-27 10:40:48 -0500

    01/27
    Commit #1

--- WeatherZipDemo/app/src/main/java/com/example/weatherzipdemo/MainActivity.java
+++ WeatherZipDemo/app/src/main/java/com/example/weatherzipdemo/MainActivity.java
@@ -5,11 +5,13 @@ import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 
 import com.example.weatherzipdemo.databinding.ActivityMainBinding;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.picasso.Picasso;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     String zip;
     URL url;
     ActivityMainBinding binding;
+    ObjectMapper mapper = new ObjectMapper();
+    JsonNode rootNode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +37,16 @@ public class MainActivity extends AppCompatActivity {
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
 
-        ObjectMapper mapper = new ObjectMapper();
 
         binding.submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try {
-                    JsonNode rootNode = mapper.readTree(run());
+                    JsonNode g = run();
                     binding.lat.setText("Lat: " + rootNode.findValue("lat").asText());
                     binding.longitude.setText("Long: " + rootNode.findValue("lon").asText());
+                    Log.d("tag",("https://raw.githubusercontent.com/isneezy/open-weather-icons/23fbe8a68faa04fa85003d15525d6b1ada746c58/src/svg/" + g.findValue("weather").findValue("icon") +".svg").replaceAll("\"",""));
+                    Picasso.with(getApplicationContext()).load(("https://raw.githubusercontent.com/isneezy/open-weather-icons/23fbe8a68faa04fa85003d15525d6b1ada746c58/src/svg/" + g.findValue("weather").findValue("icon") +".svg").replaceAll("\"","")).resize(600,600).into(binding.imageView);
                 } catch (IOException | NullPointerException e) {
                     e.printStackTrace();
                 }
@@ -49,18 +54,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public String run() throws MalformedURLException {
+    public JsonNode run() throws MalformedURLException {
         zip = binding.enterzip.getText().toString();
         try {
             url = new URL("http://api.openweathermap.org/geo/1.0/zip?zip="+ zip + ",US&appid=2fc9adf729b289e256d2bf14889cc922");
             URLConnection connection = url.openConnection();
             InputStream stream = connection.getInputStream();
             BufferedReader buff = new BufferedReader(new InputStreamReader(stream));
-            return buff.readLine();
+            rootNode = mapper.readTree(buff.readLine());
+            URL url2 = new URL("http://api.openweathermap.org/data/2.5/onecall?lat="+ rootNode.findValue("lat") + "&lon="+ rootNode.findValue("lon") + "&exclude=daily&appid=2fc9adf729b289e256d2bf14889cc922");
+            URLConnection connection2 = url2.openConnection();
+            InputStream stream2 = connection2.getInputStream();
+            BufferedReader buff2 = new BufferedReader(new InputStreamReader(stream2));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while((line = buff2.readLine()) != null)
+                sb.append(line);
+            return mapper.readTree(sb.toString());
         } catch (IOException e) {
             binding.enterzip.setHint("Enter A Valid US Zip");
             e.printStackTrace();
         }
-        return "";
+        return rootNode;
     }
 }
