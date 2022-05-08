f685cd5de3b57586293de440f266f88d8885f4b3
JoshJ <hellojosh04@gmail.com>
2022-05-08 00:10:54 -0400

    5/8 - Initial Game Dev

--- ClashRoyale/app/src/main/java/com/example/clashroyale/MainActivity.java
+++ ClashRoyale/app/src/main/java/com/example/clashroyale/MainActivity.java
@@ -1,20 +1,95 @@
 package com.example.clashroyale;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Image;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
 import android.widget.VideoView;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    MediaPlayer vp;
     MediaPlayer mp;
-    VideoView vw;
+    GameSurface gs;
+    SurfaceView sv;
+    Thread gameThread;
+    volatile boolean running = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FrameLayout root = findViewById(R.id.root);
+        gs = new GameSurface(this);
+        sv = new SurfaceView(this);
+        sv.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+                try {
+                    vp = MediaPlayer.create(getApplicationContext(), Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.gameplay2), sv.getHolder());
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+                vp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mediaPlayer) {
+                        mp.start();
+                        vp.start();
+                    }
+                });
+                vp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    @Override
+                    public void onCompletion(MediaPlayer mediaPlayer) {
+                        vp.stop();
+                        //root.removeView(sv);
+                        root.addView(gs);
+                        gs.setZOrderOnTop(true);
+                        gs.getHolder().setFormat(PixelFormat.TRANSPARENT);
+                        ImageView ig = new ImageView(getApplicationContext());
+                        ig.setImageResource(R.drawable.cr);
+                        root.addView(ig);
+                        running = true;
+                        gameThread = new Thread(gs);
+                        gameThread.start();
+                    }
+                });
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+            }
+        });
+        root.addView(sv);
+
+        sv.setZOrderOnTop(true);
+        //gs.getHolder().setFormat(PixelFormat.TRANSPARENT);
 
         mp = MediaPlayer.create(getApplicationContext(), R.raw.sudden_death_01);
         Runnable t = new Runnable() {
@@ -29,23 +104,124 @@ public class MainActivity extends AppCompatActivity {
             }
         };
 
-        vw = findViewById(R.id.videoView);
-        vw.setVideoURI(Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.gameplay));
-        vw.start();
-        vw.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-            @Override
-            public void onPrepared(MediaPlayer mediaPlayer) {
-                mediaPlayer.stop();
-                mp.setLooping(true);
-                t.run();
-            }
-        });
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        gs.pause();
         mp.stop();
         mp.release();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            gs.resume();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class GameSurface extends SurfaceView implements Runnable, SensorEventListener {
+        SurfaceHolder holder = getHolder();
+        volatile float step = 0;
+        Bitmap ball;
+        int ballX = 0;
+        Paint paint;
+        int screenw;
+        int screenh;
+        Canvas canvas;
+        int boundRight;
+        int boundLeft;
+        volatile float a = 0;
+
+
+
+        public GameSurface(Context context) {
+            super(context);
+            System.out.println("Constructed!");
+            ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+            Display disp = getWindowManager().getDefaultDisplay();
+            Point screenSize = new Point();
+            disp.getSize(screenSize);
+
+            screenw = screenSize.x;
+            screenh = screenSize.y;
+
+            paint = new Paint();
+            getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
+
+                }
+
+                @Override
+                public void surfaceChanged(@NonNull SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(@NonNull SurfaceHolder surfaceHolder) {
+
+                }
+            });
+
+
+            SensorManager sm = (SensorManager) getApplicationContext().getSystemService(SENSOR_SERVICE);
+            Sensor rot = sm.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+            sm.registerListener(this, rot, SensorManager.SENSOR_DELAY_GAME);
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent sensorEvent) {
+
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int i) {
+
+        }
+
+        @Override
+        public void run() {
+            try {
+                while (running) {
+                    System.out.println("game thread!");
+                    if (!holder.getSurface().isValid())
+                        continue;
+                    Canvas canvas = holder.lockCanvas();
+                    boundLeft = canvas.getClipBounds().left;
+                    boundRight = canvas.getClipBounds().right;
+                    paint.setTextAlign(Paint.Align.CENTER);
+                    canvas.drawText("Hello World!", (canvas.getWidth() / 2), (int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent()) / 2)), paint);
+                    paint.setTextSize(80);
+                    System.out.println(step + " " + a);
+                    if (ballX >= screenw / 2 - ball.getWidth() / 2 || ballX <= -1 * screenw / 2 + ball.getWidth() / 2) {
+                        step *= -1;
+                        System.out.println("shift");
+                    }
+                    ballX += step;
+                    canvas.drawBitmap(ball, (screenw / 2) - ball.getWidth() / 2 + ballX, (screenh / 2) - ball.getHeight(), null);
+
+                    holder.unlockCanvasAndPost(canvas);
+                }
+            } catch(Exception e){e.printStackTrace();}
+        }
+
+        public void resume() throws InterruptedException {
+            gameThread = new Thread(this);
+            gameThread.start();
+        }
+
+        public void pause(){
+            running = false;
+            while(true){
+                try{
+                    gameThread.join();
+                } catch (InterruptedException e){ }
+            }
+        }
     }
 }
---
+++ WhackDeathEater/app/src/main/java/com/example/whackdeatheater/JOSEPH_J(1).java
@@ -0,0 +1,311 @@
+package com.example.whackdeatheater;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
+
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
+import android.widget.TableRow;
+
+import com.example.whackdeatheater.databinding.ActivityMainBinding;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity{
+    ActivityMainBinding binding;
+    final ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
+    ScheduledFuture<?> cancelHandler;
+    Runnable cancel;
+    public static int timeLeft = 60;
+    final TranslateAnimation tA = new TranslateAnimation(0,0,200,0);
+    ScheduledFuture<?> moleHandler;
+    final TranslateAnimation tA2 = new TranslateAnimation(0,0,0,200);
+    final TranslateAnimation tA3 = new TranslateAnimation(0,0,0,200);
+    static int score = 0;
+    static float offset = 0;
+    static boolean voldy = false;
+    static boolean ran = false;
+    ArrayList<Integer> rows;
+    ArrayList<Integer> cols;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setTimer();
+        tA.setDuration(600);
+        tA2.setDuration(600);
+        tA3.setDuration(200);
+        rows = new ArrayList<Integer>();
+        cols = new ArrayList<Integer>();
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                int finalI = i;
+                int finalJ = j;
+                findImage(i,j).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        System.out.println("onclick");
+                        ImageView c = findImage(finalI, finalJ);
+                        System.out.println(c);
+                        c.clearAnimation();
+                        c.startAnimation(tA3);
+                        if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.cup).getConstantState()){
+                            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this);
+                            Intent intent = new Intent(MainActivity.this, BossActivity.class);
+                            intent.putExtra("score", score);
+                            startActivity(intent, options.toBundle());
+                        }
+                        if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.bellatrix).getConstantState()){
+                            generateImage(R.drawable.bellatrix);
+                        } else if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.igor).getConstantState()) {
+                            generateImage(R.drawable.igor);
+                        } else if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.lucmalfoynew).getConstantState()) {
+                            generateImage(R.drawable.lucmalfoynew);
+                        } else if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.voldy).getConstantState()) {
+                            generateImage(R.drawable.voldy);
+                        } else if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.ran).getConstantState()) {
+                            generateImage(R.drawable.ran);
+                        }
+                        if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.voldy).getConstantState()){
+                            score += 10;
+                            binding.score.setText("Score: " + score);
+                        } else if(c.getDrawable().getConstantState() == getResources().getDrawable(R.drawable.ran).getConstantState()){
+                            changeTime(-5);
+                            score -= 3;
+                            binding.score.setText("Score: " + score);
+                        } else
+                            binding.score.setText("Score: " + (++score));
+                    }
+                });
+            }
+        }
+        moleHandler = service.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    int s = (int) (Math.random() * 3) + 1;
+                    int row, col;
+                    do {
+                        row = (int) (Math.random() * 3);
+                        col = (int) (Math.random() * 3);
+                    } while (rows.contains(row) && cols.contains(col));
+                    rows.add(row);
+                    cols.add(col);
+                    ImageView curr = findImage(row, col);
+                    System.out.println("i" + row + "" + col);
+                    MainActivity.this.runOnUiThread(() -> runMole(s, curr));
+                    System.out.println("RunMole");
+                    curr.startAnimation(tA);
+                    tA.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+                            curr.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            //long currtime = System.nanoTime();
+                            //while(System.nanoTime() <= (currtime + 100)){}
+                            curr.startAnimation(tA2);
+                            rows.remove(findRow(curr));
+                            cols.remove(findCol(curr));
+                            if(curr.getDrawable().getConstantState() != getResources().getDrawable(R.drawable.cup).getConstantState())
+                                curr.setVisibility(View.INVISIBLE);
+                            System.out.println("j"+findRow(curr)+findCol(curr));
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) { }
+                    });
+                    tA2.setAnimationListener(new Animation.AnimationListener() {
+                        @Override
+                        public void onAnimationStart(Animation animation) {
+
+                        }
+
+                        @Override
+                        public void onAnimationEnd(Animation animation) {
+                            try {
+                                if (curr.getDrawable().getConstantState() != getResources().getDrawable(R.drawable.cup).getConstantState())
+                                    curr.setVisibility(View.INVISIBLE);
+                            } catch(Exception e){e.printStackTrace();}
+                        }
+
+                        @Override
+                        public void onAnimationRepeat(Animation animation) {
+
+                        }
+                    });
+                    rows.remove(Integer.valueOf(findRow(curr)));
+                    cols.remove(Integer.valueOf(findCol(curr)));
+                } catch (Exception e) {e.printStackTrace();}
+            }
+        }, 800, 1100, TimeUnit.MILLISECONDS);
+    }
+    public void setTimer(){
+        Runnable timer;
+        timer = new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(timeLeft);
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        android.os.Process.setThreadPriority(Thread.MAX_PRIORITY);
+                        if(timeLeft > 9)
+                            binding.timedisplay.setText("0:" + timeLeft);
+                        else
+                            binding.timedisplay.setText("0:0" + timeLeft);
+                        binding.progressBar.setProgress(timeLeft);
+                        LayerDrawable progressBarDrawable = (LayerDrawable) binding.progressBar.getProgressDrawable();
+                        Drawable backgroundDrawable = progressBarDrawable.getDrawable(0);
+                        Drawable progressDrawable = progressBarDrawable.getDrawable(1);
+                        String hexback = "", hexfore = "";
+                        if(timeLeft <= 15){
+                            //Gryffindor
+                            hexback = "#D3A625";
+                            hexfore = "#740001";
+                        } else if(timeLeft <= 30){
+                            //Hufflepuff
+                            hexback = "#000000";
+                            hexfore = "#FFD800";
+                        } else if(timeLeft <= 45){
+                            //Slytherin
+                            hexback = "#5D5D5D";
+                            hexfore = "#1A472A";
+                        } else {
+                            //Ravenclaw
+                            hexback = "#946B2D";
+                            hexfore = "#0E1A40";
+
+                        }
+
+                        backgroundDrawable.setTint(Color.parseColor(hexback));
+                        //binding.progressBar.setBackgroundColor(Color.parseColor(hexback));
+                        binding.progressBar.setProgressTintList(ColorStateList.valueOf(Color.parseColor(hexfore)));
+                    }
+                });
+                timeLeft--;
+            }
+        };
+        ScheduledFuture<?> handler = service.scheduleAtFixedRate(timer, 1,1, TimeUnit.SECONDS);
+        cancel = new Runnable() {
+            @Override
+            public void run() {
+                int s = 4;
+                int row, col;
+                do {
+                    row = (int) (Math.random() * 3);
+                    col = (int) (Math.random() * 3);
+                } while (rows.contains(row) && cols.contains(col));
+                rows.add(row);
+                cols.add(col);
+                ImageView curr = findImage(row, col);
+                System.out.println("i" + row + "" + col);
+                MainActivity.this.runOnUiThread(() -> runMole(s, curr));
+                System.out.println("RunMole");
+                tA.setDuration(1500);
+                curr.startAnimation(tA);
+                handler.cancel(false);
+                moleHandler.cancel(false);
+            }
+        };
+        cancelHandler = service.schedule(cancel, timeLeft, TimeUnit.SECONDS);
+    }
+    public void changeTime(int z){
+        cancelHandler.cancel(false);
+        timeLeft += z;
+        if(timeLeft < 0) timeLeft = 0;
+        cancelHandler = service.schedule(cancel, timeLeft, TimeUnit.SECONDS);
+    }
+
+    public void runMole(int s, ImageView curr){
+        switch(s){
+            case 1:
+                curr.setImageResource(R.drawable.bellatrix);
+                break;
+            case 2:
+                curr.setImageResource(R.drawable.lucmalfoynew);
+                break;
+            case 3:
+                curr.setImageResource(R.drawable.igor);
+                break;
+            case 4:
+                curr.setImageResource(R.drawable.cup);
+                return;
+        }
+        if(((int)(Math.random() * 15)) == 1 && !voldy) {
+            curr.setImageResource(R.drawable.voldy);
+            voldy = true;
+        } else if(((int)(Math.random() * 15)) == 2 && !ran){
+            curr.setImageResource(R.drawable.ran);
+            ran = true;
+        }
+    }
+    public ImageView findImage(int row, int col){
+        return (ImageView) findViewById(getResources().getIdentifier("i" + row + "" + col, "id", getApplicationContext().getPackageName()));
+    }
+    public CardView findCard(int row, int col){
+        return (CardView) (findViewById(getResources().getIdentifier("i" + row + "" + col, "id", getApplicationContext().getPackageName()))).getParent();
+    }
+    public Integer findRow(ImageView img){
+        return Integer.valueOf(binding.tableLayout.indexOfChild((TableRow) img.getParent().getParent().getParent()));
+    }
+    public Integer findCol(ImageView img){
+        return Integer.valueOf(((ViewGroup) img.getParent().getParent().getParent()).indexOfChild((ConstraintLayout) img.getParent().getParent()));
+    }
+
+    public ImageView generateImage(int resid){
+        ImageView image = new ImageView(this);
+        image.setId(View.generateViewId());
+        image.setAdjustViewBounds(true);
+        Picasso.get().load(resid).fit().into(image);
+
+        ConstraintLayout.LayoutParams params1 = new ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.MATCH_CONSTRAINT_PERCENT*30, ConstraintLayout.LayoutParams.MATCH_CONSTRAINT_PERCENT*30);
+        image.setLayoutParams(params1);
+
+        ConstraintLayout layout = binding.root;
+        layout.addView(image);
+
+        ConstraintSet constraintSet = new ConstraintSet();
+        constraintSet.clone(layout);
+
+        constraintSet.connect(image.getId(), ConstraintSet.TOP, layout.getId(), ConstraintSet.TOP);
+        constraintSet.connect(image.getId(), ConstraintSet.BOTTOM, layout.getId(), ConstraintSet.BOTTOM);
+        constraintSet.connect(image.getId(), ConstraintSet.LEFT, layout.getId(), ConstraintSet.LEFT);
+        constraintSet.connect(image.getId(), ConstraintSet.RIGHT, layout.getId(), ConstraintSet.RIGHT);
+        constraintSet.setVerticalBias(image.getId(), 0.13f + offset);
+        constraintSet.setHorizontalBias(image.getId(), 0);
+
+        offset += 0.01f;
+
+        constraintSet.applyTo(layout);
+        return image;
+    }
+}
